c6a7556a1a46d582af40a125e55d28ed43fad1f9
凶 凶 <yh13169228317@icloud.com>
2025-12-07 10:28:01 +0800

    精简优选域名列表，保留2个域名

--- _worker.js
+++ _worker.js
@@ -15,16 +15,7 @@ let scu = 'https://url.v1.mk/sub';  // 订阅转换地址
 // 默认优选域名列表
 const directDomains = [
     { name: "cloudflare.182682.xyz", domain: "cloudflare.182682.xyz" },
-    { domain: "freeyx.cloudflare88.eu.org" },
-    { domain: "bestcf.top" },
-    { domain: "cdn.2020111.xyz" },
-    { domain: "cf.0sm.com" },
-    { domain: "cf.090227.xyz" },
-    { domain: "cf.zhetengsha.eu.org" },
-    { domain: "cfip.1323123.xyz" },
-    { domain: "cloudflare-ip.mofashi.ltd" },
-    { domain: "cf.877771.xyz" },
-    { domain: "xn--b6gac.eu.org" }
+    { domain: "freeyx.cloudflare88.eu.org" }
 ];
 
 // 默认优选IP来源URL（使用自己的GitHub仓库）
